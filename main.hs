5c88a40db1f3d1feaf7e03d2affee33fc1b639ae
OKAZAKI Yasuo <cohyou@ironoir.io>
2016-05-18 15:29:41 +0900

    use State monad

--- main.hs
+++ main.hs
@@ -4,6 +4,7 @@ import Data.Char
 
 import Control.Monad.State
 
+{-
 parseTest p s = do
     print $ fst $ p s
     `catch` \(SomeException e) ->
@@ -11,11 +12,26 @@ parseTest p s = do
 
 anyChar (x:xs) = (x, xs)
 satisfy f (x:xs) | f x = (x, xs)
+-}
+
+parseTest p s = do
+    print $ evalState p s
+    `catch` \(SomeException e) ->
+        putStr $ show e
+
+anyChar :: State String Char
+anyChar = state $ anyChar where
+    anyChar (x:xs) = (x, xs)
+
+satisfy :: (Char -> Bool) -> State String Char
+satisfy f = state $ satisfy
+    where satisfy (x:xs) | f x = (x, xs)
 
 char c = satisfy (== c)
 digit  = satisfy isDigit
 letter = satisfy isLetter
 
+{-
 test3 xs0 =
     let (x1, xs1) = letter xs0
         (x2, xs2) = digit  xs1
@@ -32,20 +48,39 @@ test5 xs0 = ([x1, x2, x3], xs3)
     where (x1, xs1) = (char '[') xs0
           (x2, xs2) = digit xs1
           (x3, xs3) = (char ' ') xs2
+-}
 
-a :: State s Int
-a = return 3
+test3 = do
+    x1 <- letter
+    x2 <- digit
+    x3 <- digit
+    return [x1, x2, x3]
+
+test4 = do
+    x1 <- (char '[')
+    x2 <- digit
+    x3 <- (char ' ')
+    return [x1, x2, x3]
+
+{-
+test5 = do
+    x1 <- test1
+    x2 <- anyChar
+    return $ x1 ++ [x2]
+-}
+
+{-
+type Parsed = [Char]
+test6 :: Char -> State Parsed Char
+test6 c = state $ \(x:xs) -> (x xs)
+-}
 
 main = do
     handle <- openFile "parse.iq" ReadMode
     text <- hGetContents handle
     parseTest test4 text
-    parseTest test5 text
     parseTest test3 "abc"
     parseTest test3 "123"
     parseTest test3 "a23"
     parseTest test3 "a234"
-    print $ runState a "wowow1"
-    print $ evalState a "wowow2"
-    print $ execState a "wowow3"
     hClose handle
