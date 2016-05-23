441c9eb6a29fe551467373a19fce2565e68cd39f
cohyou <cohyou@aurola.net>
2016-05-24 06:15:17 +0900

    Add List and enable to show them

--- parse2.hs
+++ parse2.hs
@@ -1,22 +1,27 @@
-data Briq = Int8 Integer | Text String | Smbl String
+data Briq = Int8 Integer | Text String | Smbl String | List [Briq]
 
-data Annot = Annot String
+data Annt = Annt String
 
-newtype AnnotList = AnnotList { getList :: [Annot] }
+newtype AnntList = AnntList { getList :: [Annt] }
 
-data Aexp = Aexp AnnotList Briq
+data Aexp = Aexp AnntList Briq
 
 instance Show Briq where
     show (Int8 i) = show i
     show (Text s) = "\"" ++ s ++ "\""
     show (Smbl s) = s
+    show (List []) = ";"
+    show (List (x:xs)) = "[" ++ show x ++ showG xs ++ "]"
 
-instance Show Annot where
-    show (Annot s) = "@" ++ s
+showG [] = ""
+showG (x:xs) = " " ++ show x ++ showG xs
 
-instance Show AnnotList where
-    show (AnnotList []) = ""
-    show (AnnotList (x:xs)) = show x ++ " " ++ show (AnnotList xs)
+instance Show Annt where
+    show (Annt s) = "@" ++ s
+
+instance Show AnntList where
+    show (AnntList []) = ""
+    show (AnntList (x:xs)) = show x ++ " " ++ show (AnntList xs)
 
 instance Show Aexp where
     show (Aexp annot briq) = show annot ++ show briq
@@ -24,5 +29,8 @@ instance Show Aexp where
 
 main :: IO ()
 main = do
-    print $ AnnotList [(Annot "print")]
-    print $ Aexp (AnnotList [(Annot "print"), (Annot "kick")]) (Text "wowow")
+    print $ AnntList [(Annt "print")]
+    print $ Aexp (AnntList [(Annt "print"), (Annt "kick")]) (Text "wowow")
+    print $ Aexp (AnntList []) (List [])
+    print $ Aexp (AnntList []) (List [(Text "wowow"), (Smbl "make"), (Int8 53545)])
+    print $ Aexp (AnntList [(Annt "print")]) (List [(Text "wowow"), (Smbl "make"), (Int8 53545)])
