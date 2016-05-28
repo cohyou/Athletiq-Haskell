bfcebed1cb74ad51ee018eee3170a3a9ce29e62d
cohyou <cohyou@aurola.net>
2016-05-28 13:10:04 +0900

    make A-expression parser

--- parse2.hs
+++ parse2.hs
@@ -1,3 +1,6 @@
+import System.IO
+import Text.ParserCombinators.Parsec
+
 data Briq = Int8 Integer | Text String | Smbl String | List [Briq]
 
 data Annt = Annt String
@@ -26,11 +29,117 @@ instance Show AnntList where
 instance Show Aexp where
     show (Aexp annot briq) = show annot ++ show briq
 
+simple :: Parser Char
+simple = letter
+
+openClose :: Parser Char
+openClose = do { char '('; char ')'}
+
+parens :: Parser ()
+parens = do
+    char '('
+    parens
+    char ')'
+    parens
+    <|> return ()
+
+-- testOr = string "(a)" <|> string "(b)"
+{-
+testOr1 = do
+    char '('
+    char 'a' <|> char 'b'
+    char ')'
+-}
+-- testOr2 = try (string "(a)") <|> string "(b)"
+
+testOr3 = do
+    try (string "(a")
+    char ')'
+    return "(a)"
+    <|> string "(b)"
+
+nesting :: Parser Int
+nesting = do
+    char '('
+    n <- nesting
+    char ')'
+    m <- nesting
+    return (max (n+1) m)
+    <|> return 0
+{-
+word :: Parser String
+word = do
+    c <- letter
+    do
+        cs <- word
+        return (c:cs)
+        <|> return [c]
+-}
+
+word :: Parser String
+word = many1 letter
+
+sentence :: Parser [String]
+sentence = do
+    words <- endBy1 word separator
+    oneOf ".?!"
+    return words
+
+separator :: Parser ()
+separator = skipMany1 (space <|> char ',')
+
+smbl :: Parser Briq
+smbl = do
+    w1 <- many1 letter
+    wt <- many alphaNum
+    return (Smbl (w1++wt))
+
+int8 :: Parser Briq
+int8 = do
+    i <- many1 digit
+    return (Int8 (read i :: Integer))
+
+list :: Parser [Briq]
+list = sepEndBy briq spaces
+
+text :: Parser Briq
+text = do
+    try (char '"')
+    w <- many (noneOf "\"")
+    char '"'
+    return (Text w)
+
+briq :: Parser Briq
+briq = do
+    spaces
+    (do
+        text <|> smbl <|> int8
+        <|> do
+            char '['
+            spaces
+            l <- list
+            char ']'
+            return (List l))
+
+annt :: Parser Annt
+annt = do
+    char '@'
+    w <- many alphaNum
+    return (Annt w)
+
+anntlist = do
+    anntlist <- endBy annt spaces
+    return (AnntList anntlist)
+
+aexp :: Parser Aexp
+aexp = do
+    spaces
+    al <- anntlist
+    b <- briq
+    return (Aexp al b)
 
 main :: IO ()
 main = do
-    print $ AnntList [(Annt "print")]
-    print $ Aexp (AnntList [(Annt "print"), (Annt "kick")]) (Text "wowow")
-    print $ Aexp (AnntList []) (List [])
-    print $ Aexp (AnntList []) (List [(Text "wowow"), (Smbl "make"), (Int8 53545)])
-    print $ Aexp (AnntList [(Annt "print")]) (List [(Text "wowow"), (Smbl "make"), (Int8 53545)])
+    handle <- openFile "parse.iq" ReadMode
+    text <- hGetContents handle    
+    print $ parse aexp "" text
